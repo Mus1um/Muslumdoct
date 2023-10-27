d85e5fdfdb5e6e2fc472fa78aa250229e6987a97
muslummemmedzadee@gmail.com <muslummemmedzadee@gmail.com>
2023-10-28 00:18:00 +0400

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    navText: ["<img src='./img/angle-left.svg'>" , "<img src='./img/angle-left.svg'>"],
     responsive:{
         0:{
             items:1
